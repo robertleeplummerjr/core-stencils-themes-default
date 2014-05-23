e7ef0958268b4467295e78be4b4fdf49d27d1aac
Nokome Bentley <nokome.bentley@stenci.la>
2014-05-23 16:48:29 +1200

    Bypass Ace editor's handling of F6, F7 etc for swapping views

--- html-view.js
+++ html-view.js
@@ -19,6 +19,20 @@ var Stencila = (function(Stencila){
 		// Set the maximum number of lines for the code. When the number
 		// of lines exceeds this number a vertical scroll bar appears on the right
 		editor.setOption("maxLines",1000);
+		// Ace intercepts special "command" keys including the ones
+		// that we use to change views so get around that here...
+		editor.keyBinding.originalOnCommandKey = editor.keyBinding.onCommandKey;
+		editor.keyBinding.onCommandKey = function(event, hashId, keyCode) {
+			if(keyCode==117 || keyCode==118 || keyCode==119 || keyCode==120){
+				var view = Stencils.Stencil;
+				if(keyCode==117) view.change(Stencils.NormalView);
+				else if(keyCode==118) view.change(Stencils.RevealView);
+			}
+			else {
+				return this.originalOnCommandKey(event, hashId, keyCode);
+			}
+		};
+
 		// It is tricky getting ACE editor to display correctly as an overlay
 		// on top of the content
 		// So, for the time being just hide the content
