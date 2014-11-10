b7afbe171fcdcfe1e6265dc6c7a5db01305bee82
Nokome Bentley <nokome.bentley@bigsaltyblue.net>
2014-11-11 11:32:51 +1300

    Updates to console window

--- console-window.js
+++ console-window.js
@@ -12,12 +12,13 @@ var Stencila = (function(Stencila){
 			'<div class="console-window">' +
 				'<div class="close"><i class="fa fa-close"></i></div>' +
 				'<textarea class="command" type="textbox" rows="3"></textarea>' +
-				'<textarea class="result" type="textbox" rows="10"></textarea>' +
+				'<textarea class="result" type="textbox" readonly="readonly" rows="10"></textarea>' +
 			'</div>'
 		).appendTo('body');
 
 		var command = content.find('.command');
 		var result = content.find(".result");
+		// Key PRESSes
 		command.keypress(function(event){
 			// Ctrl+Enter
 			if(event.ctrlKey & event.keyCode==10){
@@ -32,6 +33,18 @@ var Stencila = (function(Stencila){
 					}
 					result.val(output);
 				});
+			}
+		});
+		// Key DOWNs
+		command.keydown(function(event){
+			// Up arrow
+			if(event.keyCode==38){
+				command.val("up");
+				// Get both the command and corresponding result from the cache
+			}
+			// Down arrow
+			else if(event.keyCode==40){
+				command.val("down");
 			}
 		});
 
